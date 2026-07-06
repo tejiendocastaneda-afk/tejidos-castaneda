33bd206635a9860cca01f5b5439d7f4bf875d338
tejiendocastaneda-afk <tejiendocastaneda@gmail.com>
2026-07-06 17:55:53 -0500

    Delete backend directory

--- backend/config/database.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-const DB_PATH = path.join(__dirname, '../../data/tejidos.db');
-const db = new Database(DB_PATH);
-db.pragma('foreign_keys = ON');
-db.pragma('journal_mode = WAL');
-module.exports = db;
